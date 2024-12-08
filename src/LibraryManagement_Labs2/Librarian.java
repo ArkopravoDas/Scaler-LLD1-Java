a4df8a9604be35e92cc4f2dda19cc69d8fb3b483
Arkopravo Das <arko.andrew2000@gmail.com>
2024-12-08 21:27:54 +0530

    Task 5 commit

--- src/LibraryManagement_Labs2/Librarian.java
+++ src/LibraryManagement_Labs2/Librarian.java
@@ -1,4 +1,31 @@
 package LibraryManagement_Labs2;
 
+import java.awt.print.Book;
+
 public class Librarian extends User{
+    private String employeeNumber;
+
+    Librarian(String employeeNumber, String name, String contactInfo){
+        super(name, contactInfo);
+        this.employeeNumber = employeeNumber;
+    }
+
+    @Override
+    public void displayDashboard(){
+        System.out.println("Librarian Dashboard");
+        System.out.println("Employee Number: " + employeeNumber + " Name: " + getName());
+    }
+
+    @Override
+    public boolean canBorrowBooks(){
+        return true;
+    }
+
+    public void addNewBook(Book book){
+        System.out.println("New Book Added");
+    }
+
+    public void removeBook(Book book){
+        System.out.println("Book Removed");
+    }
 }
